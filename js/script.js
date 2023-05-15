c6718219170602e987d43d33e4ff9c59b34d083f
jorgerios88 <jorgito.rios@hotmail.com>
2023-05-15 02:00:24 -0300

    subida de mensaje de compra

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let searchForm = document.querySelector('.header .search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-}
+// document.querySelector('#search-btn').onclick = () =>{
+//     searchForm.classList.toggle('active');
+//     navbar.classList.remove('active');
+// }
 
 let navbar = document.querySelector('.header .navbar');
 
@@ -36,17 +36,26 @@ const form = document.getElementById('login');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
   
-  if (username.value.trim() === '') {
-    alert('Por favor ingrese su nombre de usuario');
-    return;
-  }
+//   if (username.value.trim() === '') {
+//     alert('Por favor ingrese su nombre de usuario');
+//     return;
+//   }
   
-  if (password.value.trim() === '') {
-    alert('Ingrese su password');
-    return;
-  }
-  form.submit();
-});
+//   if (password.value.trim() === '') {
+//     alert('Ingrese su password');
+//     return;
+//   }
+//   form.submit();
+// });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+let nroVenta = getRandomInt(100000)
+let mensaje = `<h3>Número de venta: ${nroVenta}</h3>`
+document.getElementById(`nroVenta`).innerHTML = mensaje // + nroVenta
